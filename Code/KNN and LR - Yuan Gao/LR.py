19abd14a4bf13ec6fab987b3a557be7ab5a4f3f6
_alanglanglang <ygao61@gwu.edu>
2020-04-20 21:14:55 -0400

    KNN model and parameter tuning

--- Code/KNN and LR - Yuan Gao/LR.py
+++ Code/KNN and LR - Yuan Gao/LR.py
@@ -1,11 +1,11 @@
 import pandas as pd
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+
 # 1. read data
 train = pd.read_csv("realornot_trainencode.csv")
 test = pd.read_csv("realornot_testencode.csv")
 test_origin = pd.read_csv("test.csv")
 test_id = test_origin["id"]
-print(train.shape)
 # 2. data pre-processing and feature engineering
 train.drop("Unnamed: 0", axis=1, inplace=True)
 test.drop("Unnamed: 0", axis=1, inplace=True)
@@ -13,12 +13,12 @@ train_x = train.iloc[:, 0:-1]
 train_y = train.iloc[:, -1]
 
 # 3. modeling
-KNN= KNeighborsClassifier(n_neighbors=12,n_jobs=-1)
-KNN.fit(train_x, train_y)
+LogisticR= LogisticRegression(max_iter=80, solver="lbfgs")
+LogisticR.fit(train_x, train_y)
 
 
 # 4. prediction
-predict1 = KNN.predict(test)
+predict1 = LogisticR.predict(test)
 
 # 5. output
 result = pd.DataFrame()
@@ -26,4 +26,4 @@ result["id"] = test_id
 result["target"] = predict1
 result["id"] = result["id"].apply(lambda x: str(int(x)))
 result["target"] = result["target"].apply(lambda x: str(int(x)))
-result.to_csv("result_KNN.csv", index=False)
+result.to_csv("result_LR.csv", index=False)
