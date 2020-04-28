936adb2845e22e9b04332914a8dcfd4600ba7a67
_alanglanglang <ygao61@gwu.edu>
2020-04-28 05:27:37 -0400

    KNN model and parameter tuning

--- Code/KNN and LR - Yuan Gao/KNN_ROC_PR.py
+++ Code/KNN and LR - Yuan Gao/KNN_ROC_PR.py
@@ -1,36 +1,16 @@
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_curve, auc, precision_recall_curve, f1_score
+from sklearn.metrics import roc_curve, auc, precision_recall_curve, f1_score,average_precision_score
 import matplotlib.pyplot as plt
 
 # 1. read data
 train = pd.read_csv("realornot_trainencode.csv")
-test = pd.read_csv("realornot_testencode.csv")
-test_origin = pd.read_csv("test.csv")
-test_id = test_origin["id"]
-print(train.shape)
 
 # 2. data pre-processing and feature engineering
 train.drop("Unnamed: 0", axis=1, inplace=True)
-test.drop("Unnamed: 0", axis=1, inplace=True)
 train_x = train.iloc[:, 0:-1]
 train_y = train.iloc[:, -1]
-
-# 3. modeling
-KNN = KNeighborsClassifier(n_neighbors=12, n_jobs=-1)
-KNN.fit(train_x, train_y)
-
-# 4. prediction
-predict1 = KNN.predict(test)
-
-# 5. output
-result = pd.DataFrame()
-result["id"] = test_id
-result["target"] = predict1
-result["id"] = result["id"].apply(lambda x: str(int(x)))
-result["target"] = result["target"].apply(lambda x: str(int(x)))
-result.to_csv("result_KNN.csv", index=False)
 
 # 6. Plot ROC an PR
 # 6.1 ROC
@@ -54,7 +34,9 @@ plt.show()
 
 # 6.2 PR
 precision, recall, threshold_ = precision_recall_curve(y_test, preds)
-plt.title('Precision Recall')
+average_precision=average_precision_score(y_test, preds)
+plt.title('Precision Recall :'
+          'AP={0:0.2f}'.format(average_precision))
 plt.plot(recall, precision, "b", label="KNN")
 plt.plot([0, 1], [no_skill, no_skill], 'r--', label="No Skill")
 plt.legend(loc='upper right')
